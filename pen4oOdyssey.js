e009bdaaac9bc34a14473d8d1390442bf7e42bca
PalmiraGeorgieva <150438745+PalmiraGeorgieva@users.noreply.github.com>
2025-01-23 09:04:13 +0200

    Update pen4oOdyssey.js

--- pen4oOdyssey.js
+++ pen4oOdyssey.js
@@ -1,16 +1,17 @@
 function pen4oOdyssey(doorNumber) {
-    let tresureRoom = 3
-    if (doorNumber === 1 && doorNumber != tresureRoom) {
+    if(doorNumber !== 1 && doorNumber !== 2 && doorNumber !==3) {
+        console.log(`Invalid choice. Please select Door 1, 2, or 3.`);
+        return;
+
+    }
+
+    let treasureRoom = Math.floor(Math.random() * 3) + 1;
+
+    if (doorNumber === treasureRoom) {
+        console.log(`Congratulations, Pen4o! You found the treasure behind Door ${treasureRoom}!`)
+    } else {
         console.log(`A bucket of cold water splashes on you! Unlucky Pen4o!`)
 
-    } else if (doorNumber === 2 && doorNumber != tresureRoom) {
-        console.log (`A bucket of cold water splashes on you! Unlucky Pen4o!`)
-
-    } else if (doorNumber === 3 && doorNumber == tresureRoom) {
-       console.log(`Congratulations, Pen4o! You found the treasure behind Door ${tresureRoom}!`)
-
-    } else {
-        console.log(`Invalid choice. Please select Door 1, 2, or 3.`)
     }
 }
 pen4oOdyssey(4)
